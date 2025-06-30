0150b19882c9dec41639b67c68f42dec2f601768
JoelVerano <jowelerss@gmail.com>
2025-06-30 13:59:46 +0800

    AppBar Implemented

--- instructor_page/src/components/AvatarList.jsx
+++ instructor_page/src/components/AvatarList.jsx
@@ -1,22 +1,29 @@
 import CustomAvatar from "./CustomAvatar";
 import Avatars from "./ConfigAvatar";
 import Box from "@mui/material/Box";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from "@mui/material/Toolbar";
 
 const AvatarList = () => {
-    const leftAvatar = Avatars[0]
-    const rightAvatars = Avatars.slice(1)
+    const leftAvatar = Avatars[0];
+    const rightAvatars = Avatars.slice(1);
 
     return (
-        <Box className="avatar_cont">
-            <Box>
-                <CustomAvatar {...leftAvatar} />
-            </Box>
+        <AppBar position="sticky" sx={{ backgroundColor: '#FFFDF6'}}>
+            <Toolbar>
+                <Box sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+                    <Box>
+                        <CustomAvatar {...leftAvatar} />
+                    </Box>
+                    <Box sx={{ display: "flex", gap: 1 }}>
+                        {rightAvatars.map(({ id, src, alt, size }) => (
+                            <CustomAvatar key={id} src={src} alt={alt} size={size} />
+                        ))}
+                    </Box>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
-            <Box className="avatar_right_cont">
-                {rightAvatars.map (({ id, src, alt, size}) => (<CustomAvatar key={id} src={src} alt={alt} size={size} />))}
-            </Box>
-        </Box>
-    )
-}
-
-export default AvatarList
+export default AvatarList;
